858ecf7bd72e8c5c1ff7ca75c7a770603d3d6306
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:49:06 -0700

    refactor backend logic to separate file and export function to interface

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -1,27 +1,6 @@
-var key = require('./../.env').apiKey;
 var username = "andrewniekamp";
-      $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
-        console.log(response);
+var getUserRepos = require('./../js/ghapi.js').repoModule;
 
-        $("#user").html(
-          "<h1><a href='" + response[0].owner.html_url + "'>" + response[0].owner.login + "</a></h1>" +
-          "<a href='" + response[0].owner.html_url + "'><img id='user-image' src='" + response[0].owner.avatar_url +"'/></a>"
-        );
-
-        var repoContent =
-
-
-        $.each(response, function(index) {
-          $("#repos").append(
-            "<div class='col-sm-6'>" +
-              "<h1><a href='" + this.url + "'>" + this.name + "</a></h1>" +
-              "<div class='col-xs-12'>" +
-                "<li>" + this.created_at + "</li>" +
-              "</div>" + //closing col-xs-12
-            "</div>" + //closing row
-            "<div class='clearfix visible-sm'></div>"
-          );
-        });
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
+$(function(){
+  getUserRepos(username);
+});
--- js/ghapi.js
+++ js/ghapi.js
@@ -1,1 +1,28 @@
+var key = require('./../.env').apiKey;
 
+var getUserRepos = function(username) {
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
+    console.log(response);
+
+    $("#user").html(
+      "<h1><a href='" + response[0].owner.html_url + "'>" + response[0].owner.login + "</a></h1>" +
+      "<a href='" + response[0].owner.html_url + "'><img id='user-image' src='" + response[0].owner.avatar_url +"'/></a>"
+    );
+
+    $.each(response, function(index) {
+      $("#repos").append(
+        "<div class='col-sm-6'>" +
+        "<h1><a href='" + this.url + "'>" + this.name + "</a></h1>" +
+        "<div class='col-xs-12'>" +
+        "<li>" + this.created_at + "</li>" +
+        "</div>" + //closing col-xs-12
+        "</div>" + //closing row
+        "<div class='clearfix visible-sm'></div>"
+      );
+    });
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+}
+
+exports.repoModule = getUserRepos;
