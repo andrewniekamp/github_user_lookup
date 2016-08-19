312f3e41f43d2218462b5cdfa33f8ea5b6db4b70
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:01:31 -0700

    add initial get for repos based on username with key (stored in ignored .env)

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -1,1 +1,7 @@
-
+var key = require('./../.env').apiKey;
+var username = "andrewniekamp";
+      $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
