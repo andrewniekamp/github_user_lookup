cb69b3eb45eeb7f6ba11399dae292c55411df836
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:34:36 -0700

    add clearfix to correct column issues

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -6,14 +6,19 @@ var username = "andrewniekamp";
           "<h1>" + response[0].owner.login + "</h1>" +
           "<img id='user-image' src='" + response[0].owner.avatar_url +"'/>"
         );
+
+        var repoContent =
+
+
         $.each(response, function(index) {
           $("#repos").append(
-            "<div class='row'>" +
+            "<div class='col-sm-6'>" +
               "<h1><a href='" + this.url + "'>" + this.name + "</a></h1>" +
               "<div class='col-xs-12'>" +
                 "<li>" + this.created_at + "</li>" +
               "</div>" + //closing col-xs-12
-            "</div>" //closing row
+            "</div>" + //closing row
+            "<div class='clearfix visible-sm'></div>"
           );
         });
       }).fail(function(error){
