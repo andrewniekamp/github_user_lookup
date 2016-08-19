9d59e17a149f724e6477cc50433a19c9c3f9f7b2
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:09:03 -0700

    add output section for response's user image and profile name

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -2,6 +2,10 @@ var key = require('./../.env').apiKey;
 var username = "andrewniekamp";
       $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
         console.log(response);
+        $("#user").html(
+          "<h1>" + response[0].owner.login + "</h1>" +
+          "<img id='user-image' src='" + response[0].owner.avatar_url +"'/>"
+        );
         $.each(response, function(index) {
           $("#repos").append("<li>" + this.name + "</li>");
         });
