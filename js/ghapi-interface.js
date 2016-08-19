8ad7afe2812656cdf74436f1caf75b59a6d62e7d
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:22:15 -0700

    add links to repo in title and list date created

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -7,7 +7,14 @@ var username = "andrewniekamp";
           "<img id='user-image' src='" + response[0].owner.avatar_url +"'/>"
         );
         $.each(response, function(index) {
-          $("#repos").append("<li>" + this.name + "</li>");
+          $("#repos").append(
+            "<div class='row'>" +
+              "<h1><a href='" + this.url + "'>" + this.name + "</a></h1>" +
+              "<div class='col-xs-12'>" +
+                "<li>" + this.created_at + "</li>" +
+              "</div>" + //closing col-xs-12
+            "</div>" //closing row
+          );
         });
       }).fail(function(error){
         console.log(error.responseJSON.message);
