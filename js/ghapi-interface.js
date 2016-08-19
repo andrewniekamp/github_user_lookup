edbca5d4630dbaf19438347d8b1d635142829c47
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:07:32 -0700

    add output ul for response's list of repos

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -2,6 +2,9 @@ var key = require('./../.env').apiKey;
 var username = "andrewniekamp";
       $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
         console.log(response);
+        $.each(response, function(index) {
+          $("#repos").append("<li>" + this.name + "</li>");
+        });
       }).fail(function(error){
         console.log(error.responseJSON.message);
       });
