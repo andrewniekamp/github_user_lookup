3062d3dcb646f96e0e3823df4543f4323b7a9157
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-19 09:38:24 -0700

    add link to user url in username

--- js/ghapi-interface.js
+++ js/ghapi-interface.js
@@ -2,8 +2,9 @@ var key = require('./../.env').apiKey;
 var username = "andrewniekamp";
       $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response){
         console.log(response);
+        
         $("#user").html(
-          "<h1>" + response[0].owner.login + "</h1>" +
+          "<h1><a href='" + response[0].owner.html_url + "'>" + response[0].owner.login + "</a></h1>" +
           "<img id='user-image' src='" + response[0].owner.avatar_url +"'/>"
         );
 
